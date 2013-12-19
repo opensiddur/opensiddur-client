7eb588f15d692474d4c60acbf82d58c02438db7b
Efraim Feinstein <efraim.feinstein@gmail.com>
2013-12-19 01:27:57 -0800

    load texts on selection

--- js/controllers/textsctrl.js
+++ js/controllers/textsctrl.js
@@ -8,17 +8,54 @@ OpenSiddurClientApp.controller(
     'TextsCtrl',
     ['$scope', '$http', 'XsltService', 'AuthenticationService',
     function ($scope, $http, XsltService, AuthenticationService) {
-        console.log("Texts controller. Nothing to do here");
+        console.log("Texts controller.");
+        $scope.errorMessage = "";
         $scope.editor = {
             loggedIn : AuthenticationService.loggedIn,
             currentDocument : null,
+            content : "",
             isNew : 0,
             newDocument : function() {
                 console.log("Start a new document");
+            },
+            setDocument : function(toDocument) {
+                if (toDocument == "") {
+                    this.newDocument();
+                }
+                else {
+                    $http.get(toDocument + "/flat") 
+                        .success(
+                            function(data) {
+                                transformed = XsltService.transformString("teiToHtml", data);
+                                console.log(transformed);
+                                $scope.editor.content = (new XMLSerializer()).serializeToString(transformed);
+                                $scope.editor.isNew = 0;
+                                $scope.errorMessage = "";
+                                $scope.textsForm.$setPristine();
+                            }
+                        )
+                        .error(
+                            function(data) {
+                                $scope.errorMessage = getApiError(data);
+                                console.log("error loading", toDocument);
+                                $scope.editor.currentDocument = "";
+                            }
+                        )
+                }
             }
         };
         $scope.saveButtonText = function() {
             return this.textsForm.$pristine ? (($scope.editor.isNew) ? "Unsaved, No changes" : "Saved" ) : "Save";
         };
+
+        $scope.$watch("editor.currentDocument",
+            function(newDoc, oldDoc) {
+                $scope.editor.setDocument(newDoc);
+            }
+        );
+
+        if ($scope.editor.loggedIn) {
+            $scope.editor.currentDocument = "";
+        }
     }]
 );
