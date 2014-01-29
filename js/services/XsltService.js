a31d49a4da64758be2f87283ac374fd5c6628837
Efraim Feinstein <efraim.feinstein@gmail.com>
2014-01-28 23:49:25 -0800

    add dependency on Saxon CE; new wordify XSLT (2.0)

--- js/services/XsltService.js
+++ js/services/XsltService.js
@@ -11,61 +11,19 @@ OpenSiddurClientApp.service(
         svc = {
             xsltProcessors : {},
             addProcessor : function ( processorName, path ) {
-                thisSvc = this;
-         
-                var xslDomDoc = Sarissa.getDomDocument();
-                $.ajax({
-                    url : path,
-                    success : function(data) {
-                        xslDomDoc = (new DOMParser()).parseFromString(data, "text/xml");  
-                        var processor = new XSLTProcessor();
-                        processor.importStylesheet(xslDomDoc);
-                        thisSvc.xsltProcessors[processorName] = processor;
-                        console.log("XSLT service:" + processorName + " added");
-                    },
-                    error : function() {
-                        console.error("Could not load XSLT " + processorName + " from " + path);
-                    },
-                    async : false
-                });    
-                /*
-                return $http.get(path)
-                    .success(
-                        function( data, status, headers, config ) {
-                            var xslDomDoc = Sarissa.getDomDocument();
-                            xslDomDoc = (new DOMParser()).parseFromString(data, "text/xml");  
-                            
-                            var processor = new XSLTProcessor();
-                            processor.importStylesheet(xslDomDoc);
-                            thisSvc.xsltProcessors[processorName] = processor;
-                            console.log("XSLT service:" + processorName + " added");
-                        } )
-                    .error(
-                        function( data, status, headers, config ) {
-                            console.error("Could not load XSLT " + processorName + " from " + path);
-                        }
-                    )
-                
-                */
+                this.xsltProcessors[processorName] = path; 
             },
             transform : function ( processorName, domDoc, parameters ) {
-                if (parameters) {
-                    thisProcessor = this.xsltProcessors[processorName];
-                    //xsltCopy = angular.copy(this.xsltProcessors[processorName]);
-                    //console.log(xsltCopy);
-                    for (var parameter in parameters) {
-                        thisProcessor.setParameter(null, parameter, parameters[parameter]);
-                    }
-                    transformed = thisProcessor.transformToDocument(domDoc);
-                    thisProcessor.clearParameters();
-                    return transformed;
-                }
-                else 
-                    return this.xsltProcessors[processorName].transformToDocument(domDoc);
+                var transformed = Saxon.run({
+                    stylesheet : this.xsltProcessors[processorName],
+                    source : domDoc,
+                    parameters : parameters,
+                    method : "transformToDocument"
+                });
+                return transformed.getResultDocument();
             },
             transformString : function ( processorName, data, parameters ) {
-                var dataDomDoc = Sarissa.getDomDocument();
-                dataDomDoc = (new DOMParser()).parseFromString(data, 'text/xml');
+                var dataDomDoc = Saxon.parseXML(data);
                 return this.transform(processorName, dataDomDoc, parameters);
             }
         }
@@ -76,6 +34,7 @@ OpenSiddurClientApp.service(
         svc.addProcessor('sourceFormTemplate', '/xsl/sourceformtemplate.xsl');
         svc.addProcessor('cleanupForm', '/xsl/cleanupform.xsl');
         svc.addProcessor('addXmlId', '/xsl/add-xml-id.xsl');
+        svc.addProcessor('wordify', '/xsl/wordify.xsl');
         return svc;
     }]
 );
---
+++ xsl/wordify.xsl
@@ -0,0 +1,42 @@
+<!--
+    turn segments into word-separated entities, where required
+
+    Open Siddur Project
+    Copyright 2014 Efraim Feinstein, efraim@opensiddur.org
+    Licensed under the GNU Lesser General Public License, version 3 or later
+-->
+<xsl:stylesheet 
+    xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:tei="http://www.tei-c.org/ns/1.0"
+    xmlns:j="http://jewishliturgy.org/ns/jlptei/1.0"
+    version="2.0"
+    >
+    <xsl:output method="xml" indent="yes"/>
+
+    <xsl:template match="text()[ancestor::tei:seg][ancestor::j:streamText][not(ancestor::tei:w)][normalize-space(.)]">
+		<xsl:analyze-string select="." regex="([\p{{L}}\p{{M}}\p{{N}}\p{{S}}\p{{C}}]+)|(\p{{P}})">
+			<xsl:matching-substring>
+				<xsl:variable name="word-chars" select="regex-group(1)" as="xs:string"/>
+				<xsl:variable name="punct-chars" select="regex-group(2)" as="xs:string"/>
+				<xsl:choose>
+					<xsl:when test="$word-chars">
+						<tei:w><xsl:value-of select="$word-chars"/></tei:w>
+					</xsl:when>
+					<xsl:when test="$punct-chars">
+						<tei:pc><xsl:value-of select="$punct-chars"/></tei:pc>
+					</xsl:when>
+				</xsl:choose>
+			</xsl:matching-substring>
+			<xsl:non-matching-substring>
+			</xsl:non-matching-substring>				
+		</xsl:analyze-string>
+	</xsl:template>
+
+    <xsl:template match="comment()|*">
+        <xsl:copy>
+            <xsl:sequence select="@*"/>
+            <xsl:apply-templates/>
+        </xsl:copy>
+    </xsl:template>
+
+</xsl:stylesheet>
