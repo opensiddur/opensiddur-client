8e58ad38a892ae49feb31653e702bef635630518
Efraim Feinstein <efraim.feinstein@gmail.com>
2014-02-16 15:16:36 -0800

    prepare for ACL support

--- js/services/RestApi.js
+++ js/services/RestApi.js
@@ -44,14 +44,30 @@ OpenSiddurClientApp.factory(
                 params : { },
                 isArray : false,
                 transformResponse : function( data ) {
-                    js = x2js.xml_str2json(data);
+                    var acc = $(data);
+                    var you = acc.find("a\\:you");
+                    var aclToJs = 
+                        function ( cindex, context, attribute ) {
+                            var c = $(context);
+                            var name = c.text();
+                            var write = c.attr(attribute);
+                            return {
+                                name : write 
+                            };
+                        };
+                    var aclToJsW = function (cindex, context) { return aclToJs( cindex, context, "write"); }
+                    var aclToJsR = function (cindex, context) { return aclToJs( cindex, context, "read"); }
                     return {
-                        owner : js.access.owner.__text,
-                        group : js.access.group.__text,
-                        read : js.access.you._read == "true",
-                        write : js.access.you._write == "true",
-                        relicense : js.access.you._relicense == "true",
-                        chmod : js.access.you._chmod == "true"
+                        owner : acc.find("a\\:owner").text(),
+                        group : acc.find("a\\:group").text(),
+                        read : you.attr("read") == "true",
+                        write : you.attr("write") == "true",
+                        relicense : you.attr("relicense") == "true",
+                        chmod : you.attr("chmod") == "true",
+                        grantGroups : acc.find("a\\:grant-group").map(aclToJsW),
+                        grantUsers : acc.find("a\\:grant-user").map(aclToJsW),
+                        denyGroups : acc.find("a\\:deny-group").map(aclToJsR),
+                        denyUsers : acc.find("a\\:deny-user").map(aclToJsR)
                     };
                 }
             };
