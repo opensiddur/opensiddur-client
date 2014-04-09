8cf8c697848e19492f278a6571930c0317492570
Efraim Feinstein <efraim.feinstein@gmail.com>
2014-04-08 21:35:38 -0700

    shared data for supported licenses

--- js/app.js
+++ js/app.js
@@ -23,6 +23,15 @@ var supportedLanguages = {
     "arc" : "ארמית (Aramaic)"
 };
 
+// list of licenses supported by the app
+var supportedLicenses = {
+    "http://www.creativecommons.org/publicdomain/zero/1.0" : "Creative Commons Zero 1.0",
+    "http://www.creativecommons.org/licenses/by/4.0" : "Creative Commons Attribution 4.0",
+    "http://www.creativecommons.org/licenses/by-sa/4.0" : "Creative Commons Attribution-ShareAlike 4.0",
+    "http://www.creativecommons.org/licenses/by/3.0" : "Creative Commons Attribution 3.0 Unported",
+    "http://www.creativecommons.org/licenses/by-sa/3.0" : "Creative Commons Attribution-ShareAlike 3.0 Unported"
+};
+
 /* retrieve an API error return value and return the string */
 var getApiError = function(data) {
   return $($.parseXML(data)).find("message").text();
