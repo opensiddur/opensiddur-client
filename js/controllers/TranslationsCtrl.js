a9b44a7f2ba6d628b37f280fdea0723ee3907635
Efraim Feinstein <efraim.feinstein@gmail.com>
2014-04-27 23:03:34 -0700

    split repeated translation UI to a directive

--- js/controllers/TranslationsCtrl.js
+++ js/controllers/TranslationsCtrl.js
@@ -318,53 +318,6 @@ OpenSiddurClientApp.controller(
             },
             saveDocument : function() {
                 console.log("TBD");
-            },
-            moveSegment : function(linkage, segment, domain, direction) {
-                var linkages = this.content.linkages;
-                var linkageIdx = linkages.indexOf(linkage);
-                var segmentIdx = linkages[linkageIdx][domain].indexOf(segment);
-                console.log("Move segment: ", linkageIdx, segmentIdx, domain, direction);
-                var oppositeDomain = (domain == "left") ? "right" : "left";
-                // determine the range of segments that will move
-                var firstSegmentIdx = (direction > 0) ? segmentIdx : 0;
-                var lastSegmentIdx = (direction > 0) ? (linkages[linkageIdx][domain].length - 1) : segmentIdx;
-                // with hashkey removal...
-                var movedSegments = angular.fromJson(angular.toJson(linkages[linkageIdx][domain].splice(firstSegmentIdx, lastSegmentIdx - firstSegmentIdx + 1)));
-                // determine where to put them
-                if ((direction > 0 && linkageIdx == linkages.length - 1) || 
-                    ((direction < 0) && linkageIdx == 0 )) {
-                    // insert in a new linkage block after|before
-                    var newlb = $scope.editor.newLinkageBlock();
-                    newlb[domain] = movedSegments;
-                    if (direction > 0) {
-                        linkages.push(newlb);
-                    }
-                    else {
-                        linkages.splice(0, 0, newlb);
-                    }
-                }
-                else if (linkages[linkageIdx][oppositeDomain].length == 0) {
-                    // insert in the next|previous existing linkage block
-                    if (direction > 0) {
-                        linkages[linkageIdx + direction][domain] = movedSegments.concat(linkages[linkageIdx + direction][domain]);
-                    }
-                    else {
-                        linkages[linkageIdx + direction][domain] = linkages[linkageIdx + direction][domain].concat(movedSegments);
-                    }
-                }
-                else {
-                    // insert in a new linkage block below|above, update linkageIdx
-                    var newlb = $scope.editor.newLinkageBlock();
-                    var delta = (direction < 0) ? 0 : 1;
-                    newlb[domain] = movedSegments;
-                    linkages.splice(linkageIdx + delta, 0, newlb);
-                    linkageIdx += delta; 
-                }
-
-                // if linkages[linkageIdx] is empty in both right and left side, remove it
-                if (linkages[linkageIdx].left.length == 0 && linkages[linkageIdx].right.length == 0) {
-                    linkages.splice(linkageIdx, 1);
-                }
             }
         };
 
