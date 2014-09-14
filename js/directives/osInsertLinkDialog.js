5ecd7d4e643f018990c8cd63cc5a72b8d464e748
Efraim Feinstein <efraim.feinstein@gmail.com>
2014-09-13 23:46:49 -0700

    insert external link dialog

--- js/directives/osInsertLinkDialog.js
+++ js/directives/osInsertLinkDialog.js
@@ -21,8 +21,16 @@ OpenSiddurClientApp.directive(
                     name : "@",
                     title : "@"
                 },
-                controller: ['$scope', function ($scope) {
+                controller: ['$scope', 'RestApi', function ($scope, RestApi) {
                     console.log("In insert link dialog controller");
+
+                    $scope.typesWithAllowedRanges = ["/api/data/original"];
+                    $scope.typesWithAllowedFragments = [
+                            "/api/data/notes",
+                            "/api/data/original",
+                            "/api/data/conditionals"
+                    ];
+
 
                     $scope.links = {
                         selectedType : "/api/data/original",
@@ -33,15 +41,34 @@ OpenSiddurClientApp.directive(
                             "/api/data/sources" : "Source",
                             "/api/data/conditionals" : "Conditional"
                         },
+                        content : "",
                         selection : "",
                         selectedFile : "",
+                        composedFragment : "",
+                        allowedFragment : false,    // set by a watch on selectedType
+                        allowedRange : false,       // set by a watch on selectedType
                         selectFile : function () {
+                            // set the selected file
                             this.selectedFile = this.selection.replace(/^\/exist\/restxq\/api/, '');
+                            // if a fragment is allowed, load the content
+                            if ($scope.typesWithAllowedFragments.indexOf(this.selectedType) >= 0) {
+                                RestApi[this.selectedType].get(
+                                    { resource : decodeURI(this.selection.replace("\/exist\/restxq" + this.selectedType + "/", "")) },  // TODO: need to standardize when URIs are encoded!
+                                    function(content) {
+                                        $scope.links.content = content.xml;
+                                    } );
+                            } 
+                            this.composeLink();
+                        },
+                        composeLink : function() {
+                            this.composedLink = this.selectedFile + (this.composedFragment || "");
+                            return this.composedLink;
                         }
                     };                    
 
                     $scope.OKButton = function () {
-                        $scope.onOk()($scope.links.selectedFile);
+                        $scope.links.composeLink();
+                        $scope.onOk()($scope.links.composedLink);
                         $("#"+$scope.name).modal('hide');
                     };
                     $scope.CloseButton = function () {
@@ -56,6 +83,15 @@ OpenSiddurClientApp.directive(
                     elem.find(".modal-header h3").attr("id", scope.name + "_label");
                     elem.find(".osInsertLinkDialog").attr("aria-labelledBy", scope.name + "_label");
                     elem.find(".osInsertLinkDialog").attr("id", scope.name);
+
+
+                    scope.$watch("links.selectedType", function (type) {
+                        scope.allowedRange = scope.typesWithAllowedRanges.indexOf(type) >= 0;
+                        scope.allowedFragment = scope.typesWithAllowedFragments.indexOf(type) >= 0;
+                    });
+                    scope.$watch("links.composedFragment", function (fragment) {
+                        scope.links.composeLink();
+                    });
 
                  },
                  transclude : false,
