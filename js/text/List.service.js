930e281e73aa5c944608ae0771ebec2d169c12eb
Efraim Feinstein <efraim.feinstein@gmail.com>
2016-10-06 21:03:05 -0700

    bugfix: editing lists causes existing lists to disappear

--- js/text/List.service.js
+++ js/text/List.service.js
@@ -17,7 +17,7 @@ osTextModule.service("ListService", [
             // add a new list layer with the given id
             TextService.content(
                 XsltService.indentToString(
-                    XsltService.transformString("/js/text/Layer.add.xsl", TextService.content(), {
+                    XsltService.transformString("/js/text/Layer.add.xsl", TextService.syncFlat(), {
                         "layer-type" : "list",
                         "resource" : TextService._resourceApi + "/" + encodeURIComponent(TextService._resource),
                         "layer-id" : layerId
@@ -29,7 +29,7 @@ osTextModule.service("ListService", [
         getListLayers : function() {
             // return all existing list layer ids
             var layers = x2js.xml2json(
-                XsltService.transformString("/js/text/Layer.get.xsl", TextService.content(), {
+                XsltService.transformString("/js/text/Layer.get.xsl", TextService.syncFlat(), {
                     "layer-type" : "list"
                 })
             ).layers;
@@ -40,7 +40,7 @@ osTextModule.service("ListService", [
             // rename a list layer from one id to another
             TextService.content(
                 XsltService.indentToString(
-                    XsltService.transformString("/js/text/Layer.rename.xsl", TextService.content(), {
+                    XsltService.transformString("/js/text/Layer.rename.xsl", TextService.syncFlat(), {
                         "old-layer-id" : oldId,
                         "new-layer-id" : newId
                     }), TextService._isFlat
@@ -52,7 +52,7 @@ osTextModule.service("ListService", [
             // remove an existing list layer and all associated list items
             TextService.content(
                 XsltService.indentToString(
-                    XsltService.transformString("/js/text/Layer.remove.xsl", TextService.content(), {
+                    XsltService.transformString("/js/text/Layer.remove.xsl", TextService.syncFlat(), {
                         "layer-id" : layerId
                     }), TextService._isFlat
                 )
