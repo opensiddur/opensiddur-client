fde4a99e7a53512bafac4e5635e8c6d8f8f06866
Efraim Feinstein <efraim.feinstein@gmail.com>
2019-02-13 00:01:33 -0800

    rename instance base so opensiddur won't kill its client

--- setup/gcloud-startup-script.sh
+++ setup/gcloud-startup-script.sh
@@ -47,7 +47,7 @@ then
     export DNS_NAME="app-dev.jewishliturgy.org"
     export DB_DNS_NAME="db-dev.jewishliturgy.org";
 fi
-INSTANCE_BASE=${PROJECT}-${BRANCH//\//-}-app
+INSTANCE_BASE=${PROJECT}-app-${BRANCH//\//-}
 
 echo "Installing dynamic DNS updater to update ${DNS_NAME}..."
 cat << EOF > /etc/ddclient.conf
